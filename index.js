7027c32d95bcd17df8375a6402a3b5c11a255d2b
FardeenPanjwani <fardeenpanjwani1.com>
2018-11-21 15:11:02 +0530

    updated heroku config vars and local env vars

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const morgan = require("morgan")
+
+require('dotenv').config();
 
 const userRoute = require('./app/routes/user/user.route');
+
+app.use((req, res, next) => {
+	res.append('Access-Control-Allow-Origin', '*');
+	res.append('Access-Control-Allow-Methods', '*');
+	res.append("Access-Control-Allow-Headers", "Origin, Accept,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+	res.append('Access-Control-Allow-Credentials', true);
+	next();
+});
+
+app.use(morgan("combined"))
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
+app.use(bodyParser.json());
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(`mongodb+srv://${process.env.MON_ATL_USER}:${process.env.MON_ATL_PWD}@movies-like-0jptf.mongodb.net/test?retryWrites=true`);
 
 const PORT = process.env.PORT || 8080;
 
